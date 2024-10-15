0643d97dec8f1b3f339f3156d3ad699a9d15117e
shimauma2310 <kd1348617@st.kobedenshi.ac.jp>
2024-10-15 22:01:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,15 +21,12 @@ function startGame() {
 
     // 秘密の数字を生成
     const secretNumber = generateSecretNumber(digits);
-     console.log(`Debug: Secret Number is ${secretNumber.join('')}`); // デバッグ用に秘密の数字を表示
-
-    // 追加: secretNumberをHTMLに表示
-    document.getElementById('secretNumberDisplay').textContent = `Debug: Secret Number is ${secretNumber.join('')}`;
-    document.getElementById('debugArea').style.display = 'block';
     let startTime = Date.now();
     let timerInterval;
     
-
+ // 追加: secretNumberをHTMLに表示
+    document.getElementById('secretNumberDisplay').textContent = `Debug: Secret Number is ${secretNumber.join('')}`;
+    document.getElementById('debugArea').style.display = 'block';
 
     // ゲームエリアを表示し、ヒントや入力エリア、結果表示を初期化
     document.getElementById('gameArea').style.display = 'block';
